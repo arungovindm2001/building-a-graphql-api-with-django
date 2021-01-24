e92ef3b5ac2923d3006bbf959bca97c992cbafc8
Arun Govind M <arungovindm@gmail.com>
2021-01-25 02:40:57 +0530

    Completed building the API

---
+++ django_graphql_movies/schema.py
@@ -0,0 +1,11 @@
+import graphene
+import django_graphql_movies.movies.schema
+
+class Query(django_graphql_movies.movies.schema.Query, graphene.ObjectType):
+    pass
+
+class Mutation(django_graphql_movies.movies.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
